918cd078c8ee0a9af1c97a4bdc4c3e0b571f42d8
Hasala2002 <46789216+Hasala2002@users.noreply.github.com>
2020-05-22 19:45:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function back(){
+var val =0;
+var door =document.getElementById('door');
+door.style.width = val+'%';
+door.style.opacity = val;
+// console.log("hello")
+}
+
+function person(){
+    var door =document.getElementById('door');
+    door.style.width = "25%";
+    door.style.opacity = 1; 
+}
+
+
